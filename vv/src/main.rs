d6eec1494cc955083b585eeeeaa3bb7ab960b1f9
Yu Kobayashi <yuu.meiji.gkb@gmail.com>
2024-09-16 17:49:35 +0900

    fix: README

--- vv/src/main.rs
+++ vv/src/main.rs
@@ -1,6 +1,11 @@
 use vv::sound;
 
 fn main() {
-    let file_path = "audio.wav";
+    let file_path = "assets/audio.wav";
     sound::play(&file_path[..]);
+
+    loop {
+        println!("Hello, world!");
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    }
 }
